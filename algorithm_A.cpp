61723709c5d37b636a8dfbe40a40f9f4850cd19f
Willy Huang <willy303156@gmail.com>
2020-01-12 17:13:16 +0800

    "recursive"

--- algorithm_A.cpp
+++ algorithm_A.cpp
@@ -5,14 +5,19 @@
 
 using namespace std;
 
+#define ENEMY_WIN_GAME 200000000
 #define WIN_GAME 100000000
 #define LOSE_GAME -100000000
 #define ILLEGAL -200000000
+#define GAME_BROKEN -300000000
 #define WIN 1
 #define LOSE 2
 #define NOT_END 3
 #define CORNER 1
 #define EDGE 2
+
+#define MY true
+#define ENEMY false
 /******************************************************
  * In your algorithm, you can just use the the funcitons
  * listed by TA to get the board information.(functions 
@@ -32,39 +37,51 @@ using namespace std;
  * 3. The function that return the color fo the cell(row, col)
  * 4. The function that print out the current board statement
 *************************************************************************/
-int evaluate(int row, int col, Board board, Player player, int depth);
+int evaluate(int row, int col, Board board, Player player);
 int game_result(Board board, Player player);
 int check_adjency_critical(int row, int col, Board board, char player);
 int block_type(int row, int col);
 
-void algorithm_A(Board board, Player player, int index[]){
-
-    // cout << board.get_capacity(0, 0) << endl;
-    // cout << board.get_orbs_num(0, 0) << endl;
-    // cout << board.get_cell_color(0, 0) << endl;
-    // board.print_current_board(0, 0, 0);
-
-    //////////// Random Algorithm ////////////
-    // Here is the random algorithm for your reference, you can delete or comment it.
+int Find(int row, int col, Board board, Player player, Player enemy,int depth, int T);
+int find(int row, int col, Board board, Player player, Player enemy, bool choose, int depth,int T);
+void algorithm_A(Board board, Player player, int index[])
+{
     srand(time(NULL));
     int row, col;
-    int color = player.get_color();
-    
- /*   cin >> row >> col;
-    cout << evaluate(row, col, board, player, 0) << endl;*/
-   /* while(1){
-        row = rand() % 5;
-        col = rand() % 6;
-        if(board.get_cell_color(row, col) == color || board.get_cell_color(row, col) == 'w') break;
-    }*/
+    char color = player.get_color();
+    char enemy_color;
+    int score = ILLEGAL;
 
-    int score = LOSE_GAME;
+    if (color == RED)   enemy_color = BLUE;
+    else enemy_color = RED;
 
+    Player enemy(enemy_color);
+
+/*    for (int i = 0; i < ROW; i++)
+        for (int j = 0; j < COL; j++)
+        {
+            int new_score;
+            if (score < (new_score = Find(i, j, board, player, enemy, 0, 3)) && new_score != GAME_BROKEN)
+            {
+
+                    index[0] = i;
+                    index[1] = j;
+                    score = new_score;
+            }
+   */         
+            
+ //           cout << "A  (" << i << ',' << j << ") : " << new_score << endl;      
+   //     }
+  //      cout << "(row, col) = (" << index[0] << ',' << index[1] << ")\n";
+
+  //          cout << "A : "<< score << endl;
+  //          cout << "(row, col) = (" << index[0] << ',' << index[1] << ")\n";
+ //           system("pause");  
     for (int i = 0; i < ROW; i++)
         for (int j = 0; j < COL; j++)
         {
             int new_score;
-            if (score < (new_score = evaluate(i, j, board, player, 0)))
+            if (score < (new_score = evaluate(i, j, board, player)))
             {
 
                 index[0] = i;
@@ -78,7 +95,65 @@ void algorithm_A(Board board, Player player, int index[]){
 
 }
 
-int evaluate(int row, int col, Board board, Player player, int depth)
+
+void algorithm_F(Board board, Player player, int index[]){
+    
+/*
+    srand(time(NULL));
+    int row, col;
+    int color = player.get_color();
+
+    int score = ILLEGAL;
+
+ 
+    for (int i = 0; i < ROW; i++)
+        for (int j = 0; j < COL; j++)
+        {
+            int new_score;
+            if (score < (new_score = evaluate(i, j, board, player)))
+            {
+
+                index[0] = i;
+                index[1] = j;
+                score = new_score;
+
+            }
+        }*/
+    cout << "F\n";
+            srand(time(NULL));
+    int row, col;
+    char color = player.get_color();
+    char enemy_color;
+    int score = ILLEGAL;
+
+    if (color == RED)   enemy_color = BLUE;
+    else enemy_color = RED;
+
+    Player enemy(enemy_color);
+
+    for (int i = 0; i < ROW; i++)
+        for (int j = 0; j < COL; j++)
+        {
+            int new_score;
+            if (score < (new_score = find(i, j, board, player, enemy, ENEMY, 0, 1)) && new_score != GAME_BROKEN)
+            {
+
+                    index[0] = i;
+                    index[1] = j;
+                    score = new_score;
+            }
+            
+            
+  //          cout << "F  (" << i << ',' << j << ") : " << new_score << endl;      
+        }
+ //       cout << "(row, col) = (" << index[0] << ',' << index[1] << ")\n";
+ //       cout << "F:" << score << endl;
+  //          system("pause");  
+//    system("pause");
+
+}
+
+int evaluate(int row, int col, Board board, Player player)
 {
     int score = 0;
     char player_color = player.get_color();
@@ -153,7 +228,7 @@ int check_adjency_critical(int row, int col, Board board, char player)
     if (col < COL - 1) 
         if (board.get_cell_color(row, col + 1) != player && board.get_cell_color(row, col + 1) != 'w')
             if (board.get_orbs_num(row, col + 1) == board.get_capacity(row, col + 1) - 1)
-                score -= 5 - board.get_capacity(row, col + 1);// right
+                score -= 5 - board.get_capacity(row, col + 1);  // right
 
     return score;
 }
@@ -168,4 +243,157 @@ int block_type(int row, int col)
         else return EDGE;            // edge
     }
     return 0;
+}
+
+bool place_legal(int row, int col, Board board, Player player)
+{
+    if (board.get_cell_color(row, col) != player.get_color() && board.get_cell_color(row, col) != 'w')
+        return false;
+    else return true;
+}
+
+int Find(int row, int col, Board board, Player player, Player enemy, int depth, int T)
+{
+    char player_color;
+
+    if (depth % 2 == 0) 
+    {
+        
+        player_color = player.get_color();
+        if (board.get_cell_color(row, col) != player_color && board.get_cell_color(row, col) != 'w')
+            return ILLEGAL;
+        else board.place_orb(row, col, &player);
+    }
+
+    int score = ILLEGAL;
+
+        
+    if (depth >= T - 1)     // achieve recurrsive depth
+    {
+   //     cout << "A\n";
+        for (int i = 0; i < ROW; i++)        // highest score
+            for (int j = 0; j < COL; j++)
+            {
+                int new_score;
+                if (score < (new_score = evaluate(i, j, board, player)))
+                    score = new_score;         
+            }
+        return score;
+    }
+
+               
+    if (depth % 2 == 0)     // enemy turn
+    {
+        int position[2] = {0, 0};
+        for (int i = 0; i < ROW; i++)       // find least score
+            for (int j = 0; j < COL; j++)
+            {
+                int new_score;
+     /*           if (counter >= 55 && T == 5 && i == 3 && j == 5)
+                {
+
+                    board.print_current_board(3, 5, 0);
+                    system("pause");
+                }*/
+   /*             if (counter >= 55 && T == 5 && i == 4 && j == 0)
+                {
+                    Board board_copy = board;
+                    board.print_current_board(4, 0, 0);
+                    cout << board.get_cell_color(4,0) << endl;
+                     cout << enemy.get_color() << endl;
+                     board_copy.place_orb(4, 0, &enemy);
+                     cout << board.get_cell_color(4,0) << endl;
+                     cout << enemy.get_color() << endl;
+                     board_copy.print_current_board(4, 0, 1);
+             //       system("pause");
+                }*/
+                if (score < (new_score = evaluate(i, j, board, enemy)) && new_score != ILLEGAL)
+                {
+                    score = new_score;
+                    position[0] = i;
+                    position[1] = j;
+                }
+                
+            }
+         
+        board.place_orb(position[0], position[1], &enemy);
+
+
+        if (game_result(board, player) == LOSE)   return LOSE_GAME;
+        depth++;
+    }
+    score = ILLEGAL;
+    // my turn
+    
+    for (int i = 0; i < ROW; i++)        // highest score
+        for (int j = 0; j < COL; j++)
+        {
+            int new_score;
+            if (score < (new_score = Find(i, j, board, player, enemy, depth + 1, T)))
+                score = new_score;         
+        }
+    return score;
+    
+}
+
+int find(int row, int col, Board board, Player player, Player enemy, bool choose, int depth,int T)
+{
+    int score = ILLEGAL;
+    if (depth == T)
+    {
+        if (place_legal(row, col, board, enemy))   board.place_orb(row, col, &enemy);
+        else return GAME_BROKEN;
+        if (game_result(board, player) == WIN)  return WIN_GAME;
+        else if (game_result(board, player) == LOSE)  return LOSE_GAME;
+        for (int i = 0; i < ROW; i++)
+            for (int j = 0; j < COL; j++)
+            {
+                int new_score;
+                if (score < (new_score = evaluate(i, j, board, player)))
+                {
+                    score = new_score;
+                }
+            }
+        return score;
+    }
+    if (choose == MY)
+    {
+        if (place_legal(row, col, board, enemy))   board.place_orb(row, col, &enemy);
+        else return GAME_BROKEN;
+
+        if (game_result(board, player) == WIN)  return WIN_GAME;
+        else if (game_result(board, player) == LOSE)  return LOSE_GAME;
+
+        for (int i = 0; i < ROW; i++)
+            for (int j = 0; j < COL; j++)
+            {
+                int new_score;
+                if (score < (new_score = find(i, j, board, player, enemy, !choose, depth + 1, T)) && new_score != GAME_BROKEN)
+                {
+                    score = new_score;
+                }
+            }
+        return score;
+    
+    }
+    if (choose == ENEMY)
+    {
+        if (place_legal(row, col, board, player))   board.place_orb(row, col, &player);
+        else return GAME_BROKEN;
+         if (game_result(board, player) == WIN)  return WIN_GAME;
+        else if (game_result(board, player) == LOSE)  return LOSE_GAME;
+        score = ENEMY_WIN_GAME;
+        for (int i = 0; i < ROW; i++)
+            for (int j = 0; j < COL; j++)
+            {
+                int new_score;
+                if (score > (new_score = find(i, j, board, player, enemy, !choose, depth + 1, T)) && new_score >= ILLEGAL)
+                {
+                    score = new_score;
+                }
+            }
+        return score;
+    
+    }
+    return score;
 }
